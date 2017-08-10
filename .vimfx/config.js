1452f801fa564a4be24905e3de590d9a39f3d108
BuildTools <unconfigured@null.spigotmc.org>
2017-08-10 14:14:59 -0500

    added toggle ignore mode

--- .vimfx/config.js
+++ .vimfx/config.js
@@ -8,4 +8,18 @@ vimfx.addCommand({
 })
 vimfx.set('custom.mode.normal.test','gm')
 
+//toggles ignore mode with i in addition to shift-escape
+MAPPINGS = {
+    'exit': ['<i>', 'mode.ignore'],
+},
+Object.entries(MAPPINGS).forEach(([command, value]) => {
+    let [shortcuts, mode] = Array.isArray(value)
+        ? value
+        : [value, 'mode.normal']
+    vimfx.set(`${mode}.${command}`, shortcuts)
+})
+
 // ctrl-shift-j opens developer console in firefox
+// gB will open a dialogue to blacklist certain sites
+// you can use wildcards here (*)
+// https://pastebin.com/ytw9tH63
