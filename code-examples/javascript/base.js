3f038342bc9c270660d08101fa92a1640149bb96
Sam Christy <samchristy3454998@gmail.com>
2018-06-29 16:05:32 -0400

    normal upkeep

--- code-examples/javascript/base.js
+++ code-examples/javascript/base.js
@@ -1,26 +1,29 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var x=150;
+var y=150;
+var rot=2*Math.PI/3;
 count=0;
 ctx.beginPath();
-ctx.moveTo(75,75);
+ctx.moveTo(x,y);
 function clear(){
 	count=0;
 	ctx.beginPath();
-	ctx.moveTo(75,75);
+	ctx.moveTo(x,y);
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
 function f(){
 	len=count;
-	ctx.lineTo(75+len*Math.cos(count*3*Math.PI/2),75+len*Math.sin(count*3*Math.PI/2));
+	ctx.lineTo(x+len*Math.cos(count*rot),y+len*Math.sin(count*rot));
 	ctx.stroke();
 	count+=1.01
 }
 main=setInterval(function(){
-f()
-if(count>100){
-clear();
-}
+	f()
+	if(count>300000){
+		//clear();
+	}
 }, 10);
 
 
